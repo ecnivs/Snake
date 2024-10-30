1d1ae4ba66beff20c38c961d884853159aa7c5d8
Vince Swu <vinceswu88@gmail.com>
2024-10-30 11:03:04 +0530

    loaded files

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame as pg
+from settings import *
+from pygame.math import Vector2
 import sys
 
 class Snake:
     def __init__(self):
         pg.init()
-        self.cell_size = 40
-        self.cell_number = 20
+        self.cell_size = CELL_SIZE
+        self.cell_number = CELL_NUMBER
         self.screen = pg.display.set_mode((self.cell_number * self.cell_size, self.cell_number * self.cell_size))
         
         self.clock = pg.time.Clock()
@@ -13,9 +15,38 @@ class Snake:
         self.time = 0
 
         self.is_running = True
+        self.on_init()
+
+    def on_init(self):
+        self.body = [Vector2(5,10),Vector2(4,10),Vector2(3,10)]
+        self.direction =  Vector2(1,0)
+
+        self.head_up = pg.image.load(HEAD_UP).convert_alpha()
+        self.head_down = pg.image.load(HEAD_DOWN).convert_alpha()
+        self.head_right = pg.image.load(HEAD_RIGHT).convert_alpha()
+        self.head_left = pg.image.load(HEAD_LEFT).convert_alpha()
+
+        self.tail_up = pg.image.load(TAIL_UP).convert_alpha()
+        self.tail_down = pg.image.load(TAIL_DOWN).convert_alpha()
+        self.tail_right = pg.image.load(TAIL_RIGHT).convert_alpha()
+        self.tail_left = pg.image.load(TAIL_LEFT).convert_alpha()  
+
+        
+        self.body_vertical = pg.image.load(BODY_VER).convert_alpha()
+        self.body_horizontal = pg.image.load(BODY_HOR).convert_alpha()
+
+        
+        self.body_tr = pg.image.load(BODY_TR).convert_alpha()
+        self.body_tl = pg.image.load(BODY_TL).convert_alpha()
+        self.body_br = pg.image.load(BODY_BR).convert_alpha()
+        self.body_bl = pg.image.load(BODY_BL).convert_alpha()
+        self.crunch_sound = pg.mixer.Sound(CRUNCH_WAV)
+
+        self.apple = pg.image.load(APPLE).convert_alpha()
+        self.font = pg.font.Font(FONT, 25)
 
     def update(self):
-        self.delta_time = self.clock.tick(60)
+        self.delta_time = self.clock.tick(FPS)
         self.time = pg.time.get_ticks() * 0.001
         pg.display.set_caption(f'{self.clock.get_fps() :.0f}')
 
---
+++ settings.py
@@ -0,0 +1,29 @@
+# game settings
+CELL_SIZE = 40
+CELL_NUMBER = 20
+
+FPS = 60
+
+# path
+HEAD_UP = 'assets/head_up.png'
+HEAD_DOWN = 'assets/head_down.png'
+HEAD_RIGHT = 'assets/head_right.png'
+HEAD_LEFT = 'assets/head_left.png'
+
+TAIL_UP = 'assets/tail_up.png'
+TAIL_DOWN = 'assets/tail_down.png'
+TAIL_RIGHT = 'assets/tail_right.png'
+TAIL_LEFT = 'assets/tail_left.png'
+
+BODY_VER = 'assets/body_vertical.png'
+BODY_HOR = 'assets/body_horizontal.png'
+
+BODY_TR = 'assets/body_tr.png'
+BODY_TL = 'assets/body_tl.png'
+BODY_BR = 'assets/body_br.png'
+BODY_BL = 'assets/body_bl.png'
+
+CRUNCH_WAV = 'audio/crunch.wav'
+
+APPLE = 'assets/apple.png'
+FONT = 'font/PoetsenOne-Regular.ttf'

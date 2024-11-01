cbbba7a831bc7b73463772589a599487fea0f421
Vince Swu <vinceswu88@gmail.com>
2024-11-01 22:19:31 +0530

    cleaned up code

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class Core:
     def on_init(self):
         self.snake = Snake(self.screen)
         self.fruit = Fruit(self.screen)
-        self.grass_color = (167,209,61)
+        self.grass_color = (167, 209, 61)
         self.font = pg.font.Font(FONT, 25)
 
     def render(self):
@@ -43,33 +43,33 @@ class Core:
                 self.fruit.randomize()
 
     def check_fail(self):
-        if not 0 <= self.snake.body[0].x <= CELL_NUMBER or not 0<= self.snake.body[0].y <= CELL_NUMBER:
+        if not 0 <= self.snake.body[0].x <= CELL_NUMBER or not 0 <= self.snake.body[0].y <= CELL_NUMBER:
             self.is_running = False
         for block in self.snake.body[1:]:
-            if self.snake.body[0] == block : 
+            if self.snake.body[0] == block :
                 self.is_running = False
 
     def draw_grass(self):
         for row in range(CELL_NUMBER):
-            if row % 2 == 0:  
+            if row % 2 == 0:
                 for col in range(CELL_NUMBER):
                     if col % 2 == 0:
-                        grass_rect = pg.Rect(col * CELL_SIZE,row * CELL_SIZE,CELL_SIZE,CELL_SIZE)
-                        pg.draw.rect(self.screen,self.grass_color,grass_rect)
+                        grass_rect = pg.Rect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+                        pg.draw.rect(self.screen, self.grass_color, grass_rect)
             else:
                 for col in range(CELL_NUMBER):
                     if col % 2 != 0:
-                        grass_rect = pg.Rect(col * CELL_SIZE,row * CELL_SIZE,CELL_SIZE,CELL_SIZE)
-                        pg.draw.rect(self.screen,self.grass_color,grass_rect)
+                        grass_rect = pg.Rect(col * CELL_SIZE, row * CELL_SIZE, CELL_SIZE, CELL_SIZE)
+                        pg.draw.rect(self.screen, self.grass_color, grass_rect)
 
     def draw_score(self):
         score_text = str(len(self.snake.body) - 3)
-        score_surface = self.font.render(score_text,True,(56,47,12))
+        score_surface = self.font.render(score_text, True,(56, 47, 12))
         score_rect = score_surface.get_rect(center = (50, 30))
-        apple_rect = self.fruit.apple.get_rect(midright = (score_rect.left,score_rect.centery))
+        apple_rect = self.fruit.apple.get_rect(midright = (score_rect.left, score_rect.centery))
 
-        self.screen.blit(score_surface,score_rect)
-        self.screen.blit(self.fruit.apple,apple_rect)
+        self.screen.blit(score_surface, score_rect)
+        self.screen.blit(self.fruit.apple, apple_rect)
 
     def update(self):
         self.snake.move_snake()
@@ -103,7 +103,6 @@ class Core:
             if keys[pg.K_RIGHT] and self.snake.direction.x != -1:
                 self.snake.direction = Vector2(1, 0)
                 self.last_direction_change_time = current_time
-
         pg.display.update()
 
     def run(self):
@@ -116,5 +115,5 @@ class Core:
 if __name__ == "__main__":
     core = Core()
     SCREEN_UPDATE = pg.USEREVENT
-    pg.time.set_timer(SCREEN_UPDATE,150)
+    pg.time.set_timer(SCREEN_UPDATE, 150)
     core.run()
--- snake.py
+++ snake.py
@@ -3,8 +3,8 @@ from settings import *
 class Snake:
     def __init__(self, screen):
         self.screen = screen
-        self.body = [Vector2(5,10),Vector2(4,10),Vector2(3,10)]
-        self.direction =  Vector2(1,0)
+        self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)]
+        self.direction =  Vector2(1, 0)
         self.new_block = False
         self.on_init()
 
@@ -37,57 +37,57 @@ class Snake:
             y_pos = int(block.y * CELL_SIZE)
             block_rect = pg.Rect(x_pos, y_pos, CELL_SIZE, CELL_SIZE)
             if index == 0:
-                self.screen.blit(self.head,block_rect)
+                self.screen.blit(self.head, block_rect)
             elif index == len(self.body) - 1:
-                self.screen.blit(self.tail,block_rect)
+                self.screen.blit(self.tail, block_rect)
             else:
                 previous_block = self.body[index + 1] - block
                 next_block = self.body[index - 1] - block
                 if previous_block.x == next_block.x:
-                    self.screen.blit(self.body_vertical,block_rect)
+                    self.screen.blit(self.body_vertical, block_rect)
                 elif previous_block.y == next_block.y:
-                    self.screen.blit(self.body_horizontal,block_rect)
+                    self.screen.blit(self.body_horizontal, block_rect)
                 else:
                     if previous_block.x == -1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == -1:
-                        self.screen.blit(self.body_tl,block_rect)
+                        self.screen.blit(self.body_tl, block_rect)
                     elif previous_block.x == -1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == -1:
-                        self.screen.blit(self.body_bl,block_rect)
+                        self.screen.blit(self.body_bl, block_rect)
                     elif previous_block.x == 1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == 1:
-                        self.screen.blit(self.body_tr,block_rect)
+                        self.screen.blit(self.body_tr, block_rect)
                     elif previous_block.x == 1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == 1:
-                        self.screen.blit(self.body_br,block_rect)
+                        self.screen.blit(self.body_br, block_rect)
 
     def update_head_graphics(self):
         head_relation = self.body[1] - self.body[0]
-        if head_relation == Vector2(1,0):
+        if head_relation == Vector2(1, 0):
             self.head = self.head_left
-        elif head_relation == Vector2(-1,0):
+        elif head_relation == Vector2(-1, 0):
             self.head = self.head_right
-        elif head_relation == Vector2(0,1):
+        elif head_relation == Vector2(0, 1):
             self.head = self.head_up
-        elif head_relation == Vector2(0,-1):
+        elif head_relation == Vector2(0, -1):
             self.head = self.head_down
 
     def update_tail_graphics(self):
         tail_relation = self.body[-2] - self.body[-1]
-        if tail_relation == Vector2(1,0):
+        if tail_relation == Vector2(1, 0):
             self.tail = self.tail_left
-        elif tail_relation == Vector2(-1,0):
+        elif tail_relation == Vector2(-1, 0):
             self.tail = self.tail_right
-        elif tail_relation == Vector2(0,1):
+        elif tail_relation == Vector2(0, 1):
             self.tail = self.tail_up
-        elif tail_relation == Vector2(0,-1):
+        elif tail_relation == Vector2(0, -1):
             self.tail = self.tail_down
 
     def move_snake(self):
         if self.new_block == True:
             body_copy = self.body[:]
-            body_copy.insert(0,body_copy[0] + self.direction)
+            body_copy.insert(0, body_copy[0] + self.direction)
             self.body = body_copy[:]
-            self.new_block=False
+            self.new_block = False
         else:
             body_copy = self.body[:-1]
-            body_copy.insert(0,body_copy[0] + self.direction)
+            body_copy.insert(0, body_copy[0] + self.direction)
             self.body = body_copy[:]
 
     def add_block(self):
